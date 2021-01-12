6e59e7c83496715c9ae68d59b52c0245c60ea936
JJ Sermon <thejoshuasermon@gmail.com>
2021-01-11 20:43:49 -0500

    case switch

--- main.js
+++ main.js
@@ -148,3 +148,24 @@ const y = 11;
 if (X > 5 && Y > 10) {
     console.log('x is more than 5 or y is more than 10')
 }
+
+// case switch
+
+const x = 10;
+
+const color = x > 10 ? 'red' : 'blue';
+
+console.log(color);
+
+switch(color) {
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+    default:
+        console.log('color os NOT red or blue');
+        break;
+
+}
